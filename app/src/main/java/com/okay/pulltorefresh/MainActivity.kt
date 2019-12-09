b65609f286883fbbbd2dead6795ec1b24f9ad8f3
zhaoyinglong <zhaoyinglong@okjiaoyu.cn>
2019-12-09 15:39:30 +0800

    加上拉加载功能

--- app/src/main/java/com/okay/pulltorefresh/MainActivity.kt
+++ app/src/main/java/com/okay/pulltorefresh/MainActivity.kt
@@ -6,19 +6,42 @@ import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.okay.library.PullState
 import com.okay.library.callback.RefreshListener
+import com.okay.loadlibrary.LoadMoreWrapper
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var list = Data.getList().subList(0, 20)
 
+    val myAdapter = MyAdapter(list)
+
     var headView:HeadView? = null
+
+    // use for demo, please ignore
+    private var mShowLoadFailedEnabled = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycleView.layoutManager = LinearLayoutManager(this)
-        recycleView.adapter = MyAdapter(list)
+        recycleView.adapter = myAdapter
+        LoadMoreWrapper.with(myAdapter).setShowNoMoreEnabled(true).setListener {
+            val itemCount = myAdapter.itemCount
+            if (itemCount >= 20 && mShowLoadFailedEnabled) {
+                mShowLoadFailedEnabled = false
+                recycleView.postDelayed(Runnable { it.setLoadFailed(true) }, 800)
+            } else {
+                //not enable load more
+                if (itemCount >= 40) {
+                    it.loadMoreEnabled = false
+                }
+
+                recycleView.postDelayed(Runnable {
+                    list.addAll(Data.getList().subList(20, 40))
+                    myAdapter.update(list)
+                }, 1200)
+            }
+        }.into(recycleView)
 //        headView = HeadView(this)
 //        pullLayout.addHeadView(headView!!)
         pullLayout.setOnRefreshListener(object : RefreshListener {
@@ -32,6 +55,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun mockNet() {
         pullLayout.postDelayed({
+            var list = Data.getList().subList(0, 20)
+            myAdapter.update(list)
             pullLayout.setState(PullState.REFRESH_DONE)
         }, 2000)
     }
